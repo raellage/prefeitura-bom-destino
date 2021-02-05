1c15db393d26121663fe02be438317fbfc10c936
Israel Lage <israel.lage@everymind.com.br>
2021-02-05 20:30:52 -0300

    contact form

--- src/pages/faleconosco.jsx
+++ src/pages/faleconosco.jsx
@@ -6,10 +6,26 @@ import { Layout, Container } from 'layouts';
 
 const FaleConosco = center => (
   <Layout>
-    <Helmet title={'About Page'} />
-    <Header title="About Page">Prefeitura de Bom Destino</Header>
+    <Helmet title={'Fale conosco'} />
+    <Header title="Fale conosco">Prefeitura de Bom Destino</Header>
     <Container center={center}>
-      
+      <form name="contact" method="POST" data-netlify="true">
+        <p>
+          <label>Nome: <input type="text" name="name" /></label>   
+        </p>
+        <p>
+          <label>Email: <input type="email" name="email" /></label>
+        </p>
+        <p>
+          <label>Assunto: <input type="subject" name="subject" /></label>
+        </p>
+        <p>
+          <label>Mensagem: <textarea name="message"></textarea></label>
+        </p>
+        <p>
+          <button type="submit">Enviar</button>
+        </p>
+      </form>
     </Container>
   </Layout>
 );
