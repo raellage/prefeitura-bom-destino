0e3acaabc458a842591bf30a7384175f286645d6
Israel Lage <lage.israel@hotmail.com>
2021-02-06 11:05:01 -0300

    ajuste serviço de iptu

--- src/pages/servicos/consulta-iptu-itr.jsx
+++ src/pages/servicos/consulta-iptu-itr.jsx
@@ -8,7 +8,8 @@ import { Layout, Container } from 'layouts';
 const ConsultaIPTU = center => {
 
   const [numeroCadastro, setNumeroCadastro] = useState("")
-  const [dadosImovel, setDadosImovel] = useState(0)
+  const [dadosImovel, setDadosImovel] = useState("")
+  const [dadosDebitos, setDadosDebitos] = useState([])
 
   useEffect(() => {
     
@@ -41,6 +42,7 @@ const ConsultaIPTU = center => {
         .then(res => {
           console.log(res.data);
           setDadosImovel(res.data);
+          setDadosDebitos([]);
         })
         .catch(function (error) {
           console.log(error);
@@ -48,6 +50,33 @@ const ConsultaIPTU = center => {
     }    
   }
 
+  const handleSubmitConsultarDebitos = (evt) => {
+    evt.preventDefault();
+
+    const token = localStorage.getItem('token');
+      
+    const api = `https://sgm-app.herokuapp.com/api/debitos`
+    axios.get(api, { headers: {"Authorization" : `Bearer ${token}`} })
+      .then(res => {
+        console.log(res.data);
+        let debitosImovel = res.data.filter((debito) => {
+          if(debito.imovel != null) {
+            return debito.imovel.id == numeroCadastro;
+          }
+        })
+
+        if(debitosImovel.length > 0) {
+          debitosImovel.sort(function(a, b){return b.anoReferencia - a.anoReferencia});
+        }
+
+        setDadosDebitos(debitosImovel)
+
+        console.log(debitosImovel);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
   
   return (
     <Layout>
@@ -56,7 +85,7 @@ const ConsultaIPTU = center => {
       <Container center={center}>
         <form onSubmit={handleSubmit}>
           <p>
-            <label>Número de Cadastro: 
+            <label>Digite o Número de Cadastro do Imóvel: (Ex: 1051)<br></br><br></br>
               <input 
                 type="text" 
                 name="numeroCadastro" 
@@ -64,18 +93,47 @@ const ConsultaIPTU = center => {
                 onChange={e => setNumeroCadastro(e.target.value)}
               />
             </label>
-            <input type="submit" value="Consultar" />
+            <input type="submit" value="Consultar detalhes" />
           </p>
         </form>
-        <p>Número de Cadastro: {dadosImovel.id}</p>
-        <p>Largura: {dadosImovel.largura}</p>
-        <p>Comprimento: {dadosImovel.comprimento}</p>
-        <p>Area: {dadosImovel.area}</p>
-        <p>Zona: {dadosImovel.zona}</p>
-        <p>Tipo: {dadosImovel.tipo}</p>
-        <p>Latitude: {dadosImovel.latitude}</p>
-        <p>Longitude: {dadosImovel.longitude}</p>
-        <p>CEP: {dadosImovel.endereco}</p>
+        <br></br>
+        <div>    
+          <p><b>Número de Cadastro:</b> {dadosImovel.id}</p>
+          <p><b>Largura:</b> {dadosImovel.largura}</p>
+          <p><b>Comprimento:</b> {dadosImovel.comprimento}</p>
+          <p><b>Area:</b> {dadosImovel.area}</p>
+          <p><b>Zona:</b> {dadosImovel.zona}</p>
+          <p><b>Tipo:</b> {dadosImovel.tipo}</p>
+          <p><b>Latitude:</b> {dadosImovel.latitude}</p>
+          <p><b>Longitude:</b> {dadosImovel.longitude}</p>
+        </div>
+        <br></br>
+        <div> 
+          <form onSubmit={handleSubmitConsultarDebitos}>
+            <input type="submit" value="Consultar Débitos" />     
+          </form>
+        </div>
+
+        <div>
+          <table>
+            <tbody>
+              <tr>
+                <th>Ano de Referência</th>
+                <th>Valor</th>
+                <th>Situação</th>
+              </tr>
+              {dadosDebitos.map(( value, index ) => {
+                return (                  
+                  <tr key={index}>
+                    <td>{value.anoReferencia}</td>
+                    <td>R${value.valor}</td>
+                    <td>{value.status}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </Container>
     </Layout>
   )
