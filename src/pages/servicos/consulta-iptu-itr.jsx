e3bc97ba95a34dec271e8f4d0dd0442214504096
Israel Lage <lage.israel@hotmail.com>
2021-02-06 01:53:52 -0300

    iptu service

--- src/pages/servicos/consulta-iptu-itr.jsx
+++ src/pages/servicos/consulta-iptu-itr.jsx
@@ -6,7 +6,10 @@ import { Header } from 'components';
 import { Layout, Container } from 'layouts';
 
 const ConsultaIPTU = center => {
+
+  const [numeroCadastro, setNumeroCadastro] = useState("")
   const [dadosImovel, setDadosImovel] = useState(0)
+
   useEffect(() => {
     
     const backendUrl = `https://sgm-app.herokuapp.com/api/authenticate`;
@@ -18,30 +21,61 @@ const ConsultaIPTU = center => {
       })
       .then(response => {
         console.log(response);
-        const token = response.data.id_token;
-        
-        const api = `https://sgm-app.herokuapp.com/api/imovels/1`
-        axios.get(api, { headers: {"Authorization" : `Bearer ${token}`} })
-          .then(res => {
-            console.log(res.data);
-            setDadosImovel(res.data);
-          });
+        localStorage.setItem('token', response.data.id_token)
       })
       .catch(function (error) {
         console.log(error);
       });
   }, [])
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+
+    if(numeroCadastro == "") {
+      alert(`Favor preencher o número de cadastro`);
+    } else {
+      const token = localStorage.getItem('token');
+        
+      const api = `https://sgm-app.herokuapp.com/api/imovels/${numeroCadastro}`
+      axios.get(api, { headers: {"Authorization" : `Bearer ${token}`} })
+        .then(res => {
+          console.log(res.data);
+          setDadosImovel(res.data);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }    
+  }
+
+  
   return (
     <Layout>
       <Helmet title={'Consulta IPTU/ITR - Prefeitura de Bom Destino'} />
       <Header title="Consulta IPTU/ITR">Prefeitura de Bom Destino</Header>
       <Container center={center}>
-        <form name="contact" method="POST" data-netlify="true">
+        <form onSubmit={handleSubmit}>
           <p>
-            <label>Número de Cadastro: <input type="text" name="name" /></label>   
+            <label>Número de Cadastro: 
+              <input 
+                type="text" 
+                name="numeroCadastro" 
+                value={numeroCadastro} 
+                onChange={e => setNumeroCadastro(e.target.value)}
+              />
+            </label>
+            <input type="submit" value="Consultar" />
           </p>
-          <p>Número de Cadastro: {dadosImovel.id}</p>
         </form>
+        <p>Número de Cadastro: {dadosImovel.id}</p>
+        <p>Largura: {dadosImovel.largura}</p>
+        <p>Comprimento: {dadosImovel.comprimento}</p>
+        <p>Area: {dadosImovel.area}</p>
+        <p>Zona: {dadosImovel.zona}</p>
+        <p>Tipo: {dadosImovel.tipo}</p>
+        <p>Latitude: {dadosImovel.latitude}</p>
+        <p>Longitude: {dadosImovel.longitude}</p>
+        <p>CEP: {dadosImovel.endereco}</p>
       </Container>
     </Layout>
   )
